7d9148eb822f46cca025ad876709f8aa72de02e2
Lalit Jangir <lalitjangir2001@gmail.com>
2026-07-26 11:19:03 +0530

    Add variables and data types lesson

---
+++ 01_Basics/02_Variables.py
@@ -0,0 +1,12 @@
+name = "Lalit Jangir"
+age = 24
+height = 5.11
+is_student = False
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_student))
+print(name)
+print(age)
+print(height)
+print(is_student)
